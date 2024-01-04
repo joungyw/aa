bbbad37b1ac40858fe344aff52c8f94e57e1032e
joungyw <ceojoung@naver.com>
2024-01-04 14:47:07 +0900

    123

---
+++ java_work/ex01/src/ex01/Main.java
@@ -0,0 +1,16 @@
+package ex01;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] arge) {
+		
+		Scanner scan = new Scanner(System.in);
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		
+		System.out.println(a+b);
+		
+	}
+
+}
