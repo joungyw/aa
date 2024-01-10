3ba389bd1adec7323c343f293e63191832bbea34
joungyw <155594500+joungyw@users.noreply.github.com>
2024-01-10 10:35:44 +0900

    [level unrated] Title: 나이 출력, Time: 0.02 ms, Memory: 79 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120820. 나이 출력/나이 출력.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int age) {
+        int answer = 2023 - age;
+        return answer;
+    }
+ public static void main(String[] arge){
+        
+           int age = 40;
+        
+        System.out.println(2023-age);
+    }
+}
