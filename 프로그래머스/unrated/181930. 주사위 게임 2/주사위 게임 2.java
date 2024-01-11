666f928570a5109e64241e2d4a8dd951a83bb7d2
joungyw <155594500+joungyw@users.noreply.github.com>
2024-01-11 09:59:13 +0900

    [level unrated] Title: 주사위 게임 2, Time: 0.02 ms, Memory: 81.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.java
@@ -0,0 +1,20 @@
+class Solution {
+		public static int solution(int a, int b, int c) {
+			int answer = 0;
+			
+			if (a == b && a == c) {
+				answer = (a+b+c)*((a*a)+(b*b)+(c*c))*((a*a*a)+(b*b*b)+(c*c*c));
+				System.out.println("세개 다 같음");
+			}
+			else if(a==b ||a==c ||b==c) {
+				answer = (a+b+c)*((a*a)+(b*b)+(c*c));
+				
+				
+			}else {
+				answer = (a+b+c);
+			}
+			
+
+			return answer;
+		}
+}
