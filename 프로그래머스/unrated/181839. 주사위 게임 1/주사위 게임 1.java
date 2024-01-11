3cc727d61ab0e20f1ed186e92e1d834a988de274
joungyw <155594500+joungyw@users.noreply.github.com>
2024-01-11 13:11:00 +0900

    [level unrated] Title: 주사위 게임 1, Time: 0.03 ms, Memory: 65.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.java
@@ -0,0 +1,20 @@
+class Solution {
+    public static int solution(int a, int b) {
+        int answer = 0;
+        
+        if(a%2 != 0 && b%2 !=0) 
+    	 answer = (a*a)+(b*b); 
+    	
+    	else if(a%2 != 0 || b%2 !=0) 
+    	 answer = 2*(a+b); 
+     
+    	
+    	else if(a>b && a%2 == 0 && b%2 ==0)
+    	 answer = a- b;
+        else if(a<b && a%2 == 0 && b%2 ==0)
+    	 answer = b- a;
+    	
+        
+        return answer;
+    }
+}
