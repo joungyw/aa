0ceb9b30a78c63f8303434736c70dfb340b7ae05
joungyw <155594500+joungyw@users.noreply.github.com>
2024-01-17 13:19:49 +0900

    [Bronze III] Title: 최소, 최대, Time: 2500 ms, Memory: 323280 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int N = scan.nextInt();
+		int[] arr = new int[N];
+		
+
+		for (int i = 0; i < N; i++) {
+			int num = scan.nextInt();
+			arr[i] = num;}
+		
+			{Arrays.sort(arr);
+			int a = arr[0];
+			int b = arr[arr.length - 1];
+			
+			
+			System.out.println(a);
+			System.out.println(b);
+			
+	
+		}
+			}
+		
+	
+	}
